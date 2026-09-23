fd58624ac7bbd68f66395e1dd10236229726b348
Lulushu12 <Lulushu12@users.noreply.github.com>
2026-09-23 21:01:52 +0000

    deploy: 80627549c44cd6749e2ee0ce24aecab570da32c0

---
+++ chess/assets/index-B2cDShOY.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BbN_m6FW.js","assets/index-BkwqkPIe.js","assets/index-BTpJP5Y3.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-BkwqkPIe.js";const _=p("App",{web:()=>r(()=>import("./web-BbN_m6FW.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
