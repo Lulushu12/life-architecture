e3abb22be636c20eff3820c948637b38f6c25264
Lulushu12 <Lulushu12@users.noreply.github.com>
2026-09-23 20:50:36 +0000

    deploy: f106d2c5dbb6e4c7e1125070b34e18658cd22a95

---
+++ breathe/assets/index-BhEmam9f.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BE9Ug2nO.js","assets/index-DkfzKkgV.js","assets/index-CoKzQHw5.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-DkfzKkgV.js";const _=p("App",{web:()=>r(()=>import("./web-BE9Ug2nO.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
