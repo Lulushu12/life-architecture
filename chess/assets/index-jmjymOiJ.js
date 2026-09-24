ff2b219127a7598145c57a8b2b83ba1d9d29bf4d
Lulushu12 <Lulushu12@users.noreply.github.com>
2026-09-24 05:04:31 +0000

    deploy: feadec4c6f94c048e89230a09556f59d09d90df5

---
+++ chess/assets/index-jmjymOiJ.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-6ExRf2hk.js","assets/index-CedDGdlw.js","assets/index-Detwkwgs.css"])))=>i.map(i=>d[i]);
+import{F as p,_ as r}from"./index-CedDGdlw.js";const _=p("App",{web:()=>r(()=>import("./web-6ExRf2hk.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
