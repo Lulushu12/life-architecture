7247aaf61c6cae9f673b78dd8694a3a496755d35
Lulushu12 <Lulushu12@users.noreply.github.com>
2026-09-24 05:01:46 +0000

    deploy: 0770550eae8817c8060f743dedd82c6feeb37c28

---
+++ chess/assets/index-CbPSePDj.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-D5GXagfs.js","assets/index-D0QoP8Qk.js","assets/index-BTpJP5Y3.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-D0QoP8Qk.js";const _=p("App",{web:()=>r(()=>import("./web-D5GXagfs.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
--- ortho/assets/index-D6K5HOJV.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-D8w3MHKm.js","assets/index-yDQ5Ygcz.js","assets/index-D4NMbvUq.css"])))=>i.map(i=>d[i]);
-import{r as p,_ as r}from"./index-yDQ5Ygcz.js";const _=p("App",{web:()=>r(()=>import("./web-D8w3MHKm.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
