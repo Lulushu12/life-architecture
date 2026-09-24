d819bfb3bd0567f51307803b8d58e4666fa13b5c
Lulushu12 <Lulushu12@users.noreply.github.com>
2026-09-24 05:02:35 +0000

    deploy: e64947aa58673c655e2187b8f6aafd6e78c81899

--- calories/assets/index-B6jSgfxm.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-dvZVsHxe.js","assets/index-BXXXgbPZ.js","assets/index-DQOV3WDj.css"])))=>i.map(i=>d[i]);
-import{r as p,_ as r}from"./index-BXXXgbPZ.js";const _=p("App",{web:()=>r(()=>import("./web-dvZVsHxe.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
---
+++ calories/assets/index-CTnXFVHo.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BBsWcO3M.js","assets/index-NLw2dWOQ.js","assets/index-CZg6kjk4.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-NLw2dWOQ.js";const _=p("App",{web:()=>r(()=>import("./web-BBsWcO3M.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
